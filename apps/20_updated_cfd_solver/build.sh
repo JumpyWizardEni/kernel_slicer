de2f51276c6546228b499e27f5ae5980d9a9bcc5
Timofei Klokov <tima.klokov@mail.ru>
2022-04-20 20:39:46 +0300

    build.sh changes

--- apps/20_updated_cfd_solver/build.sh
+++ apps/20_updated_cfd_solver/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd ../../
+./kslicer "apps/20_updated_cfd_solver/src/test_class.cpp" -mainClass Solver -stdlibfolder TINYSTL -pattern ipv -Iapps/LiteMath IncludeToShaders -shaderCC GLSL -DKERNEL_SLICER -v -ITINYSTL ignorecc
+cd apps/20_updated_cfd_solver
 rm -r shaders_generated
 mkdir shaders_generated
 mv src/shaders_generated/* shaders_generated/
